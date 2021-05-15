6ff6a6028ddb3f0e5807dce80a0117ba723dc6fa
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2021-05-15 15:56:27 +0700

    :wrench: add user profile to api.py

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from app.deliveries import (
         item_controller,
         user_controller,
+        user_profile_controller,
     )
 from app.middlewares import auth
 
@@ -14,6 +15,12 @@ api.include_router(
 api.include_router(
     item_controller.router,
     tags=["items"],
+    dependencies=[Depends(auth.get_current_active_user)],
+    responses={404: {"description": "Not found"}},
+)
+api.include_router(
+    user_profile_controller.router,
+    tags=["user_profiles"],
     dependencies=[Depends(auth.get_current_active_user)],
     responses={404: {"description": "Not found"}},
 )
